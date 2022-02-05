d5f8196e4c380d5ed290ad559e91cb4393f92953
Connice Trimmingham <connicetrimmingham@Connices-MacBook-Pro.local>
2022-02-05 17:56:37 -0600

    commit message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    // BEM naming convention 
+    <div className="aspp">
+      <h1> Building the facebook clone! </h1>
     </div>
   );
 }
+
 
 export default App;
---
+++ techused.js
@@ -0,0 +1,8 @@
+//Tech used for facebook clone :
+// 1. react  + rech hooks 
+//firebase hosting
+//firebox
+// react flipmove ... create animation 
+
+
+
